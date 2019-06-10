3b25980dc127d4a3fdf33c29c6c8b6717e8bc767
James Wang <jnwang@suse.com>
2019-06-10 20:17:06 +0800

    Move mitigaions_list to each mitigation pm file

--- lib/Mitigation.pm
+++ lib/Mitigation.pm
@@ -44,158 +44,12 @@ sub reboot_and_wait {
 }
 
 our $syspath = '/sys/devices/system/cpu/vulnerabilities/';
-my @mitigations_list = (
-	{
-		name => "meltdown",
-		CPUID => hex '20000000',
-		IA32_ARCH_CAPABILITIES => 1, #bit0 -- RDCL_NO
-		parameter => 'pti',
-		cpuflags => ['pti'],
-		sysfs => {
-			"on" => "Mitigation: PTI", 
-			"off" => "Vulnerable", 
-			"auto" => "Mitigation: PTI",
-			"default" => "Mitigation: PTI", 
-		},
-		dmesg => {
-			"on" => "Kernel/User page tables isolation: enabled", 
-			"off" => "", 
-			"auto" => "Kernel/User page tables isolation: enabled",
-			"default" => "Kernel/User page tables isolation: enabled", 
-		},
-		cmdline => [
-			"on",
-			"off",
-			"auto",
-		],
-		lscpu => {
-			"on" => "pti", 
-			"off" => "", 
-			"auto" => "pti",
-		},
-	},
-	{
-		name => "spectre_v2",
-		CPUID => hex '4000000',
-		IA32_ARCH_CAPABILITIES => 2, #bit1 -- EIBRS
-		parameter => 'spectre_v2',
-		cpuflags => ['ibrs', 'ibpb', 'stibp'],
-		sysfs => {
-				"on" => "Mitigation: Indirect Branch Restricted Speculation.*IBPB: always-on, IBRS_FW, STIBP: forced*", 
-				"off" => "Vulnerable,.*IBPB: disabled,.*STIBP: disabled", 
-				"auto" => "Mitigation: Indirect Branch Restricted Speculation.*IBPB: conditional, IBRS_FW, STIBP: conditional,*",
-				"retpoline" => "Mitigation: Full generic retpoline.*",
-				"ibrs" => "Mitigation: Indirect Branch Restricted Speculation.*"
-				},
-		cmdline => [
-				"on",
-				"off",
-				"auto",
-				"retpoline",
-				],
-	},
-	{
-		name => "spectre_v2_user",
-		CPUID => hex 'C000000',
-		IA32_ARCH_CAPABILITIES => 2, #bit1 -- EIBRS
-		parameter => 'spectre_v2',
-		cpuflags => ['ibpb', 'stibp'],
-		sysfs => {
-				"on" => "Mitigation: Indirect Branch Restricted Speculation.* IBPB: always-on,.* STIBP: forced,.*", 
-				"off" => ".*IBPB: disabled,.*STIBP: disabled", 
-				"prctl" => ".*IBPB: conditional.*STIBP: conditional.*",
-				"prctl,ibpb" => ".*IBPB: always-on.*STIBP: conditional.*",
-				"seccomp" => ".*STIBP: conditional.*",
-				"seccomp,ibpb" => ".*IBPB: always-on.*STIBP: conditional.*",
-				"auto" => "Mitigation: Indirect Branch Restricted Speculation.* IBPB: conditional,.* STIBP: conditional,.*",
-				},
-		cmdline => [
-				"on",
-				"off",
-				"prctl",
-				"prctl,ibpb",
-				"seccomp",
-				"seccomp,ibpb",
-				"auto",
-				],
-	},
-	{
-		name => "l1tf",
-		CPUID => hex '10000000',
-		IA32_ARCH_CAPABILITIES => 8, #bit3 --SKIP_L1TF_VMENTRY
-		parameter => 'l1tf',
-		cpuflags => ['flush_l1d'],
-		sysfs => {
-			"full" => "Mitigation: PTE Inversion; VMX: cache flushes, SMT disabled", 
-			"full,force" => "Mitigation: PTE Inversion; VMX: cache flushes, SMT disabled", 
-			"flush" => "Mitigation: PTE Inversion; VMX: conditional cache flushes, SMT vulnerable", 
-			"flush,nosmt" => "Mitigation: PTE Inversion; VMX: conditional cache flushes, SMT disabled", 
-			"flush,nowarn" => "Mitigation: PTE Inversion; VMX: conditional cache flushes, SMT vulnerable",
-			"off" => "Mitigation: PTE Inversion; VMX: vulnerable",
-			"default" => "Mitigation: PTE Inversion; VMX: conditional cache flushes, SMT vulnerable", 
-		},
-		cmdline => [
-			"full",
-			"full,force",
-			"flush",
-			"flush,nosmt",
-			"flush,nowarn",
-			"off",
-		],
-	},
-	{
-		name => "spectre_v4",
-		CPUID => hex '80000000',
-		IA32_ARCH_CAPABILITIES => 16, #bit4 --SSB_NO 
-		parameter => 'spectre_v4',
-		cpuflags => ['ssbd'],
-		sysfs => {
-			"on" => "Mitigation: Speculative Store Bypass disabled",
-			"off" => "Vulnerable",
-			"auto" => "Mitigation: Speculative Store Bypass disabled via prctl and seccomp", 
-			"prctl" => "Mitigation: Speculative Store Bypass disabled via prctl", 
-			"seccomp" => "Mitigation: Speculative Store Bypass disabled via prctl and seccomp",
-			"default" => "Mitigation: Speculative Store Bypass disabled via prctl and seccomp",
-		},
-		cmdline => [
-			"on",
-			"off",
-			"auto",
-			"prctl",
-			"seccomp",
-		],
-	},
-	{
-		name => "mds",
-		CPUID => hex '20000000',
-		IA32_ARCH_CAPABILITIES => 32, #bit5 --MDS_NO
-		parameter => 'mds',
-		cpuflags => ['md_clear'],
-		sysfs => {
-			"full" => "Mitigation: Clear CPU buffers; SMT vulnerable",
-			"full,nosmt" => "Mitigation: Clear CPU buffers; SMT disabled",
-			"off" => "Vulnerable; SMT vulnerable", 
-			"default" => "Mitigation: Clear CPU buffers; SMT vulnerable",
-		},
-		cmdline => [
-			"full",
-			"full,nosmt",
-			"off",
-		],
-	},
-);
 
 sub new{
 
 	my $class = shift;
 
-	my $self = {
-		'name' => shift,
-		'CPUID' => shift,
-		'IA32_ARCH_CAPABILITIES' => shift,
-    		'parameter' => shift,
-    		'sysfs_name' => shift
-	};
+	my $self = shift;
 
 	bless $self, $class;
 
@@ -227,35 +81,29 @@ sub Sysfs {
 }
 sub CPUID {
 	my ($self, $value) = @_;
-	if (@_ == 2) {
-		$self->{'CPUID'} = $value;
-	}
 	return $self->{'CPUID'};
 }
 
 sub MSR {
 	my ($self, $value) = @_;
-	if (@_ == 2) {
-		$self->{'IA32_ARCH_CAPABILITIES'} = $value;
-	}
 	return $self->{'IA32_ARCH_CAPABILITIES'};
 }
 
-sub load_cpuid {
+sub read_cpuid {
 	my $self = shift;
 	zypper_call("in cpuid");
 	my $edx = hex script_output(
 		"cpuid -1 -l 7 -s 0 -r | awk \'{print \$6}\' | awk -F \"=\" \'{print \$2}\' | tail -n1"
 	);
-	$self->CPUID($edx);
+	return $edx;
 }
 
-sub load_msr {
+sub read_msr {
 	my $self = shift;
 	my $edx = script_output(
 		"perl -e \'open(M,\"<\",\"/dev/cpu/0/msr\") and seek(M,0x10a,0) and read(M,\$_,8) and print\' | od -t u8 -A n"
 	);
-	$self->MSR($edx);
+	return $edx;
 }
 
 
@@ -268,11 +116,8 @@ sub show {
 
 sub vulnerabilities {
 	my $self = shift;
-	my $item;
-	foreach $item (@mitigations_list) {
-		if ($item->{'name'} eq $self->Name()) {
-			if ($item->{'CPUID'} & $self->CPUID()) {
-				if ($item->{'IA32_ARCH_CAPABILITIES'} & $self->MSR()) {
+			if ($self->read_cpuid() & $self->CPUID()) {
+				if ($self->read_msr() & $self->MSR()) {
 					record_info("Not Affected", "This machine needn't be test.");
 					return 0; #Not Affected
 				}else {
@@ -282,37 +127,25 @@ sub vulnerabilities {
 			}
 			record_info("vulnerable", "This machine need to test.");
 			return 1;
-		}
-	}
 }
 
 sub sysfs{
 	my ($self,$value) = @_;
-	my $item;
 	my $p;
-	foreach $item (@mitigations_list) {
-		if ($item->{'name'} eq $self->Name()) {
 			if (@_ == 2) {
-  				return $item->{'sysfs'}->{$value};
+  				return $self->{'mitigations_list'}->{'sysfs'}->{$value};
 			}
-  			return $item->{'sysfs'};
+  			return $self->{'sysfs'};
 
-		}
-	}
 }
 
 sub dmesg{
 	my $self = shift;
-  my $item;
   my $p;
-  foreach $item (@mitigations_list) {
-		if ($item->{'name'} eq $self->Name()) {
       for $p (keys %{$item->{'dmesg'}}) {
           print "dmesg ",$self->Name,"\n";
           print $item->{'dmesg'}->{$p},"\n";
       }
-    }
-  }
 }
 
 sub cmdline{
@@ -447,8 +280,6 @@ sub remove_parameter{
 sub do_test {
 	my $self = shift;
 	#load current cpu info
-	$self->load_msr();
-	$self->load_cpuid();
 	#check applicability
 	my $ret = $self->vulnerabilities();
 	if ($ret == 0) {
